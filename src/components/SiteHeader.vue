08b06cfa3a0ea163d5f9cb176e27426d96ca43f6
robertolatini31 <robertolatini31@gmail.com>
2022-05-05 14:30:31 +0200

    added header

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <img src="https://grafica-facile.com/wp-content/uploads/2022/01/spotify-logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+name: 'SiteHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: #2E3A46;
+    img {
+    width: 80px;
+    padding: 1rem;
+    } 
+}
+
+</style>
