2903fdedf2ea4bb42dc86bf6717c9023806892b3
robertolatini31 <robertolatini31@gmail.com>
2022-05-06 13:37:14 +0200

    added input

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,6 +1,13 @@
 <template>
   <header>
-      <img src="https://grafica-facile.com/wp-content/uploads/2022/01/spotify-logo.png" alt="">
+      <div class="my_container d-flex justify-content-between align-items-center p-3">
+        <img src="https://grafica-facile.com/wp-content/uploads/2022/01/spotify-logo.png" alt="">
+      <form class="d-flex align-items-center">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      </div>
+      <!-- /.my_container -->
   </header>
 </template>
 
@@ -14,9 +21,15 @@ name: 'SiteHeader'
 header {
     background-color: #2E3A46;
     img {
-    width: 80px;
-    padding: 1rem;
+    width: 60px;
     } 
+    form {
+      height: 30px;
+    }
+    button {
+      border-color: #1ED760!important;
+      color: #1ED760!important;
+    }
 }
 
 </style>
